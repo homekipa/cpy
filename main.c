0a959d4dc9d0f9649056dd164ba31c24dc28b00e
fkillrra <f.killrra@gmail.com>
2020-07-13 10:53:29 -0700

    result

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 #include <fcntl.h>
 #include <dirent.h> // for directory open
 //#include <unistd.h>
+#include <string.h>
 
 int check_file_size(FILE *fd);
+void cp(char *src, char *dst);
+char *reset_path(char *path, int num);
 
 int main(int argc, char *argv[])
 {
-    printf("ARGC : %d\n", argc);
+    char *dir[argc-1];
+    char *path = argv[argc-1];
+    int path_len = strlen(path);
+    int tmp;
+
+    printf("[+]---Path Len : %d---[+]\n", path_len);
+    printf("[+]---ARGC : %d---[+]\n", argc);
 
     if(argc < 3)
     {
@@ -18,55 +27,25 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    // <file1> <file2>
     if(argc == 3)
-    {
-        FILE *fd, *dest;
-        int len = 0;
+        cp(argv[1], argv[2]);
 
-        fd = fopen(argv[1], "r");
-        len = check_file_size(fd);
-        dest = fopen(argv[argc-1], "w");
-        char buf[len];
-
-        while (fread(buf, 1, len, fd))
-        {
-            buf[len] = '\0';
-            fwrite(buf, 1, len, dest);
-            printf("Length : %d\n", len);
-            printf("%s\n", buf);
-        }
-
-        fclose(dest);
-        fclose(fd);
-    }
-
-    // <file1> <file2> ... <directory>
     else
     {
-        DIR *dp;
-        struct dirent* entry = NULL;
-        struct stat dir_buf;
-
-        printf("%s is Directory!\n", argv[argc-1]);
-        if((dp = opendir(argv[argc-1])) == NULL)
+        if(path[path_len-1] != '/')
         {
-            printf("%s can't open\n");
-            return 0;
+            strcat(path, "/");
+            path_len++;
         }
 
-        while((entry = readdir(dp)) != NULL)
+        for(int i = 1; i < argc-1; i++)
         {
-            lstat(entry->d_name, &dir_buf);
-
-            if(S_ISDIR(dir_buf.st_mode))
-                printf("DIR : %s\n", entry->d_name);
-            else if(S_ISREG(dir_buf.st_mode))
-                printf("FILE : %s\n", entry->d_name);
+            tmp = strlen(argv[i]);
+            dir[i] = strcat(path, argv[i]);
+            cp(argv[i], dir[i]);
+            path = reset_path(path, tmp);
         }
-        closedir(dp);
     }
-
     return 0;
 }
 
@@ -78,4 +57,38 @@ int check_file_size(FILE *fd)
 
     printf("file size : %d\n", size);
     return size;
+}
+
+void cp(char *src, char *dst)
+{
+    FILE *src_file, *dst_file;
+    int size = 0;
+    src_file = fopen(src, "r");
+
+    size = check_file_size(src_file);
+
+    printf("File Size : %d\n", size);
+
+    char buf[size];
+    dst_file = fopen(dst, "w");
+
+    while(fread(buf, 1, size, src_file))
+    {
+        buf[size] = '\0';
+        fwrite(buf, 1, size, dst_file);
+        printf("Length : %d\n", size);
+        printf("%s\n", buf);
+    }
+    fclose(src_file);
+    fclose(dst_file);
+}
+
+char *reset_path(char *path, int num)
+{
+    int len = strlen(path);
+    for(int i = 1; i <= num; i++)
+    {
+        path[len-i] = '\0';
+    }
+    return path;
 }
